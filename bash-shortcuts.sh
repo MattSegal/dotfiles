ca31faf5e0612f1675695978672ea6654875e211
Matthew Segal <mattdsegal@gmail.com>
2018-03-17 18:25:51 +1100

    save

--- bash-shortcuts.sh
+++ bash-shortcuts.sh
@@ -1,11 +1,14 @@
+. ~/dotfiles/bash-secrets.sh
+
 alias dc="docker-compose"
+alias python=python3
 
 function github {
   branch="$(git rev-parse --abbrev-ref HEAD)"
   repo="$(git remote -v | cut -d ':' -f 2 | cut -d '.' -f 1 | head -1)"
 
   case "$1" in
-    "diff") 
+    "diff")
       url="https://github.com/${repo}/compare/${branch}"
       ;;
     *)
@@ -26,31 +29,90 @@ function manage {
     "")
       echo "Django manage wrapper"
       echo
-      echo "  reset         reset database"
-      echo "  wordpress     pull down WordPress data"
+
+      echo "  up            bring up web container"
+      echo "  debug         bring up web container with service ports"
+      echo "  reset         reset database (sdadmin password is 'z')"
       echo "  postgres      open Postgres CLI"
       echo "  lint          run linter"
+      echo "  isort         apply isort"
       echo "  test          run tests"
       echo "  shell         run shell_plus"
+      echo "  fetch         fetch prod data"
+      echo "  restore       restore local db from prod data"
+      echo "  ssh           ssh into prod, staging or backup"
+      echo "  rollback      rollback deployment to prod or staging"
       echo "  *             pass args to manage.py"
       echo
       ;;
-    "reset") 
-      docker-compose run web ./scripts/reset-db.sh
-      manage changepassword sdadmin
+    "up")
+      docker-compose up web
       ;;
-    "wordpress") 
-      args="convert_wordpress --jsonurl http://www.nextminingboom.com/wp-json/wp/v2 --homepage 3"
-      eval "docker-compose run web ./manage.py $args"
-      ;; 
+    "debug")
+      docker-compose run --service-ports web
+      ;;
+    "reset")
+      echo -ne 'z\nz\n' | docker-compose run web ./scripts/reset-db.sh
+      ;;
+    "ssh")
+      case "$2" in
+        "prod")
+          SERVER="$PROD_1_EC2"
+          ;;
+        "staging")
+          SERVER="$STAGING_EC2"
+          ;;
+        "backup")
+          SERVER="$BACKUP_EC2"
+          ;;
+        *)
+          echo "No environment specified for SSH."
+          return
+          ;;
+      esac
+      echo -e "\n\t===============\n\tSSH into $2\n\t===============\n"
+      ssh "ec2-user@$SERVER" -i $SD_SSH_KEY
+      ;;
+    "rollback")
+      case "$2" in
+        "prod")
+          echo "Rolling back prod to '${3}'"
+          ~/code/scripts/rollback-prod.sh "$3"
+          ;;
+        "staging")
+          echo "Rolling back staging to '${3}'"
+          ~/code/scripts/rollback-staging.sh "$3"
+          ;;
+        *)
+          echo "No environment specified for rollback."
+          return
+          ;;
+      esac
+      ;;
+    "fetch")
+      ~/code/scripts/fetch-prod.sh
+      ;;
+    "restore")
+      ~/code/scripts/restore-local.sh
+      ;;
     "postgres")
       psql --host=0.0.0.0 --port=25432 --username=postgres
       ;;
     "test")
-      docker-compose run web make test-python
+      case "$2" in
+        "")
+          docker-compose run web make test
+          ;;
+        *)
+          docker-compose run web pytest -s $2
+        ;;
+      esac
       ;;
     "lint")
-      docker-compose run web make lint-python
+      docker-compose run web make lint
+      ;;
+    "isort")
+      docker-compose run web isort --skip migrations --skip node_modules
       ;;
     "shell")
       docker-compose run web ./manage.py shell_plus
@@ -63,8 +125,21 @@ function manage {
 
 
 #  Notes
+# FOO=($(docker ps | grep '\-web\-'));docker exec -it $FOO /bin/bash
 #  aws ecr describe-repositories
 #  eval $(aws ecr get-login)
 #  docker pull $uri:$tag
 #  docker images
 #  docker run -i -t $image_id /bin/bash
+
+
+function loaddata {
+  # throwaway for finfeed work
+  echo -e 'yes\n' | manage reset_db && \
+  manage migrate && \
+  manage load_template_data && \
+  manage load_test_data --unsplash 50 && \
+  manage sitedata_to_wagtail_sites && \
+  echo -ne 'z\nz\n' | manage changepassword sdadmin
+  echo -ne 'z=User.objects.get(username="sdadmin");z.is_active=True;z.save()' | manage shell
+}
