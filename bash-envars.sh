73c8d6aa9022f62ee69753f186c6c12dc365b594
Matthew Segal <mattdsegal@gmail.com>
2018-03-18 12:10:24 +1100

    update install.sh, add PS1 mod

--- bash-envars.sh
+++ bash-envars.sh
@@ -1,2 +1,3 @@
 # https://github.com/magicmonty/bash-git-prompt
 GIT_PROMPT_ONLY_IN_REPO=1
+PS1=" $PS1"
--- install.sh
+++ install.sh
@@ -1,43 +1,37 @@
-#apt update
-#apt upgrade
-
-#apt install curl
-
-# Add Sublime Text's GPG key to apt
-#wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-
-# Add Docker's GPG key to apt
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-# Add Sublime Text's apt repo
-#echo "deb https://download.sublimetext.com/ apt/stable/" | 
-#  tee /etc/apt/sources.list.d/sublime-text.list
-
-# Add Docker's apt repo
-#add-apt-repository \
-#   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-#   $(lsb_release -cs) \
-#   stable"
-
-# Add Gnome's apt repo
-#add-apt-repository ppa:gnome3-team/gnome3
-
 apt update
 
-apt install terminator && \
-apt install meld && \
-apt install sublime-text && \
-apt install docker.io && \
-apt install chromium-browser && \
-#apt install shutter && \
-#apt install gnome-shell ubuntu-gnome-desktop && \
-apt install awscli
+apt install curl
+
+# Add Sublime Text's GPG key to apt
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+
+# Add Docker's GPG key to apt
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Add Sublime Text's apt repo
+echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
+
+# Add Docker's apt repo
+add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+
+# TODO - install Gnome
+apt install \
+	terminator \
+	meld \
+	sublime-text \
+	docker.io \
+	chromium-browser \
+	kolourpaint4 \
+	awscli
 
 
-# docker compose
-#COMPOSE_URL=https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m`
-#curl -L $COMPOSE_URL -o /usr/local/bin/docker-compose
-#chmod +x /usr/local/bin/docker-compose
+# Install docker compose
+COMPOSE_URL=https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m`
+curl -L $COMPOSE_URL -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 if [ ! -d ./bash-git-prompt ]; then
   git clone https://github.com/magicmonty/bash-git-prompt.git bash-git-prompt --depth=1
